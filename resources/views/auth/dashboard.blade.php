a7daee6932eff63e09ddd0c0cf5056bcbc130012
mukul <mukulsaini001@gmail.com>
2024-05-01 15:11:12 +0530

    changes

---
+++ resources/views/auth/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('auth.layouts')
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header"><strong>Dashboard</strong></div>
+            <div class="card-body">
+
+                <div class="alert alert-success">
+                    <h3>Hello {{Auth::user()->name}}</h3> 
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/auth/message.blade.php
@@ -0,0 +1,10 @@
+@if(session('success'))
+<div class="custom-alert-msg alert alert-success">
+    {{ session('success') }}
+</div>
+@endif
+@if(session('error'))
+<div class="custom-alert-msg alert alert-danger">
+    {{ session('error') }}
+</div>
+@endif
